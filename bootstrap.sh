5452b6fa2aa99c3795eac50d7195596bd87469f3
Kurt Geebelen <kurt.geebelen@mech.kuleuven.be>
2014-02-06 10:13:44 +0100

    updated inputrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,16 +11,3 @@ ln -sf ~/dotfiles/vim                ~/.vim
 #ln -sf ~/dotfiles/octave/octaverc    ~/.octaverc
 #ln -sf ~/dotfiles/tmux.conf          ~/.tmux.conf
 
-#Set command history such that when you start typing and push up, it only searches in history starting with the same string of letters
-echo "\"\\e[A\": history-search-backward" >> ~/.inputrc
-echo "\"\\e[B\": history-search-forward" >> ~/.inputrc
-# These 2 lines are for when you want Ctrl+up/down, in stead of just up/down
-#echo "\"\\e[1;5A\": history-search-backward" >> ~/.inputrc.test
-#echo "\"\\e[1;5B\": history-search-forward" >> ~/.inputrc.test
-#ctrl+left and ctrl+right for moving back and forward in whole words
-echo "\"\\e[1;5C\": forward-word" >> ~/.inputrc
-echo "\"\\e[1;5D\": backward-word" >> ~/.inputrc
-echo "\"\\e[5C\": forward-word" >> ~/.inputrc
-echo "\"\\e[5D\": backward-word" >> ~/.inputrc
-echo "\"\\e\e[C\": forward-word" >> ~/.inputrc
-echo "\"\\e\e[D\": backward-word" >> ~/.inputrc
