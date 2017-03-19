260cffb5594d94750b5eca3edb27aa5152772df7
Kurt Geebelen <kurtgeebelen@gmail.com>
2017-03-19 13:16:48 +0100

    removed ln

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,5 @@ ln -sf ~/dotfiles/gitconfig          ~/.gitconfig
 ln -sf ~/dotfiles/vim/vimrc          ~/.vimrc
 rm -rf ~/.vim
 ln -sf ~/dotfiles/vim                ~/.vim
-#ln -sf ~/dotfiles/awesome            ~/.config/awesome
-#ln -sf ~/dotfiles/i3            ~/.i3
-#ln -sf ~/dotfiles/Xresources         ~/.Xresources
-#ln -sf ~/dotfiles/octave/octaverc    ~/.octaverc
-#ln -sf ~/dotfiles/tmux.conf          ~/.tmux.conf
 sudo apt-get update
 sudo apt-get install -y vim htop compiz-plugins compizconfig-settings-manager git gnome-panel openssh-server
