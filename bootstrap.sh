6b19dc7ebd7ef61f6285a7094ebe4e5283420559
Kurt Geebelen <kurtgeebelen@gmail.com>
2018-09-11 16:16:58 +0200

    removed installation of gnome-related things

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ ln -sf ~/dotfiles/gitconfig          ~/.gitconfig
 ln -sf ~/dotfiles/vim/vimrc          ~/.vimrc
 rm -rf ~/.vim
 ln -sf ~/dotfiles/vim                ~/.vim
-sudo apt-get update
-sudo apt-get install -y vim htop compiz-plugins compizconfig-settings-manager git gnome-panel openssh-server
+#sudo apt-get update
+#sudo apt-get install -y vim htop compiz-plugins compizconfig-settings-manager git gnome-panel openssh-server
