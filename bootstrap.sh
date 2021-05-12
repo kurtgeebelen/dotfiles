b44b308622d611a6721dff6483121ee28eefd6eb
Kurt Geebelen <kurtgeebelen@gmail.com>
2021-05-12 15:37:09 +0200

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "export EDITOR=vim" >> ~/.bashrc
+sudo apt-get update
+sudo apt-get install -y vim htop git openssh-server
 ln -sf ~/dotfiles/inputrc            ~/.inputrc
 ln -sf ~/dotfiles/gitconfig          ~/.gitconfig
 ln -sf ~/dotfiles/vim/vimrc          ~/.vimrc
 rm -rf ~/.vim
 ln -sf ~/dotfiles/vim                ~/.vim
-#sudo apt-get update
-#sudo apt-get install -y vim htop compiz-plugins compizconfig-settings-manager git gnome-panel openssh-server
