6c5b03b3223e4cdaabe4c375fffe00a34e315d0f
Kurt Geebelen <kurtgeebelen@gmail.com>
2024-06-05 10:10:09 +0200

    move default location to .dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@
 echo "export EDITOR=vim" >> ~/.bashrc
 sudo apt-get update
 sudo apt-get install -y vim htop git openssh-server
-ln -sf ~/dotfiles/inputrc            ~/.inputrc
-ln -sf ~/dotfiles/gitconfig          ~/.gitconfig
-ln -sf ~/dotfiles/vim/vimrc          ~/.vimrc
+ln -sf ~/.dotfiles/inputrc            ~/.inputrc
+ln -sf ~/.dotfiles/gitconfig          ~/.gitconfig
+ln -sf ~/.dotfiles/vim/vimrc          ~/.vimrc
 rm -rf ~/.vim
-ln -sf ~/dotfiles/vim                ~/.vim
+ln -sf ~/.dotfiles/vim                ~/.vim
