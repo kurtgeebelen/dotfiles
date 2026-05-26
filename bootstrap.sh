711416a9337e1983185dc1636c12b32755775785
Kurt Geebelen <kurtgeebelen@gmail.com>
2026-05-26 19:26:49 +0200

    remove apt installs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "export EDITOR=vim" >> ~/.bashrc
-sudo apt-get update
-sudo apt-get install -y vim htop git openssh-server
+#sudo apt-get update
+#sudo apt-get install -y vim htop git openssh-server
 ln -sf ~/.dotfiles/inputrc            ~/.inputrc
 ln -sf ~/.dotfiles/gitconfig          ~/.gitconfig
 ln -sf ~/.dotfiles/vim/vimrc          ~/.vimrc
